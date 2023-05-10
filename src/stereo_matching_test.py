3b83986989e2c21342234c8f51e06349f5b4fa44
AVS1508 <aditya.11.present@gmail.com>
2023-05-10 01:58:50 -0400

    Added stereo-matching code for ShapeNet data sampled renders from depth-net

---
+++ src/stereo_matching_test.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+for obj in ['bed', 'chair']:
+    for idx in range(1,3):
+        # Load the left and right images of the stereoscopic pair
+        left_image = cv2.imread(f'./images/{obj}_{idx}_left.png', cv2.IMREAD_GRAYSCALE)
+        right_image = cv2.imread(f'./images/{obj}_{idx}_right.png', cv2.IMREAD_GRAYSCALE)
+
+        # Create a StereoBM object for stereo matching
+        stereo = cv2.StereoBM_create(
+            numDisparities=16,
+            blockSize=5,
+        )
+
+        # Perform stereo matching
+        disparity = stereo.compute(left_image, right_image)
+
+        # Normalize the disparity values for visualization
+        disparity_normalized = cv2.normalize(disparity, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # Display the disparity map
+        plt.figure(figsize=(5.12, 5.12))
+        plt.imshow(disparity_normalized, cmap='gray')
+        plt.savefig(f'./images/{obj}_{idx}_disparity.png')
+        plt.show()
