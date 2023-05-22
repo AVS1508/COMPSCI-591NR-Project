a45f4d1c5c497c9f4fb45fc81cb418d57879ca5f
AVS1508 <aditya.11.present@gmail.com>
2023-05-21 23:51:55 -0400

    Functioning MSE

--- src/stereo_matching.py
+++ src/stereo_matching.py
@@ -33,7 +33,7 @@ for obj in ['bed', 'chair']:
         plt.figure(figsize=(5.12, 5.12))
         plt.imshow(depth_normalized, cmap='gray')
         plt.savefig(f'./images/{obj}_{idx}_depth.png')
-        plt.show()
+        # plt.show()
 
         # Create the 4-channel RGB-SD image
         rgb_sd_image = np.zeros((left_image_rgb.shape[0], left_image_rgb.shape[1], 4), dtype=np.uint8)
@@ -42,3 +42,11 @@ for obj in ['bed', 'chair']:
 
         # Save the resulting image
         cv2.imwrite(f'./images/{obj}_{idx}_rgbsd.png', rgb_sd_image)
+        
+        # Load the ground truth depth map
+        ground_truth_depth = cv2.imread(f'./images/{obj}_{idx}_depth.png', cv2.IMREAD_GRAYSCALE)
+        
+        # Compute the mean squared error (MSE)
+        mse = np.mean((cv2.normalize(depth_normalized.astype(np.float32), None, 0, 1, cv2.NORM_MINMAX) - cv2.normalize(ground_truth_depth.astype(np.float32), None, 0, 1, cv2.NORM_MINMAX))**2)
+
+        print(f"Mean Squared Error (MSE): {mse}")
